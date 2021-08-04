c7ca73b6828192c17680dff2ad6fb2fa2332f94a
unknown <n.germain.work@gmail.com>
2021-08-04 15:45:06 +0200

    quest completed

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  showAge = false;
+
+  User = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleAge() {
+    this.showAge = !this.showAge
+  }
+
+}
